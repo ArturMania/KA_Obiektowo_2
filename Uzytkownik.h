be68c643e9248278f2db8a72dd3655e3705b4363
Artur Mania <m.artur93@gmail.com>
2021-09-08 12:22:37 +0200

    Add register user

--- Uzytkownik.h
+++ Uzytkownik.h
@@ -6,5 +6,13 @@ class Uzytkownik{
     int id;
     string login;
     string haslo;
+public:
+    //"GETERY I CHECKERY"
+    void ustawID(int noweId);
+    void ustawLogin(string nowyLogin);
+    void ustawHaslo(string noweHaslo);
 
+    int pobierzId();
+    string pobierzLogin();
+    string pobierzHaslo();
 };
